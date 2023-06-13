9db8041e3f22c13846169261c94d52ce3d2119e6
Nikolai <krezi3927@gmail.com>
2023-06-13 23:29:05 +0900

    21 lesson

--- main.js
+++ main.js
@@ -6,12 +6,27 @@ let personalMovieDB = {
     genres:[],
     privat: false,
 };
-const a = prompt('один из просмотренных фильмов','');
-const b = prompt('на сколько бы вы его оценили','');
-const c = prompt('один из просмотренных фильмов','');
-const d = prompt('на сколько бы вы его оценили','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i = 0; i < 2; i++){
+    const a = prompt('один из просмотренных фильмов','');
+    const b = prompt('на сколько бы вы его оценили','');
+    if(a != null & b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+if(personalMovieDB.count < 10){
+    alert('Просмотрено мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    alert('Вы классический зритель');
+}else if(personalMovieDB.count >= 30){
+    alert('Вы киноман');
+}else{
+    alert('Ошибка');
+}
+
+
 
 console.log(personalMovieDB);
+
